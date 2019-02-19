74e4ba6fa6a2b3b9a3b50ad154fec5dc3cb0316b
akankshanb <akanksha.nb@gmail.com>
2019-02-19 01:43:56 -0500

    Add files via upload

---
+++ Astar_algo.py
@@ -0,0 +1,298 @@
+graph = {'albanyNY' :{'montreal':226, 'boston':166, 'rochester':148},
+        'albanyGA' : {'tallahassee' : 120, 'macon' : 106},
+        'albuquerque' : {'elPaso' : 267, 'santaFe': 61},
+        'atlanta' : {'macon' : 82,'chattanooga' : 117},
+        'augusta' : {'charlotte' : 161,'savannah' : 131},
+        'austin' : {'houston' : 186,'sanAntonio':79},
+        'bakersfield' : {'losAngeles': 112,'fresno' : 107},
+        'baltimore' : {'philadelphia' : 102,'washington' : 45},
+        'batonRouge' : {'lafayette' : 50,'newOrleans' : 80},
+        'beaumont' : {'houston' : 69,'lafayette' : 122},
+        'boise' : {'saltLakeCity' : 349,'portland' : 428},
+        'boston' : {'providence' : 51, 'albanyNY' : 166},
+        'buffalo' : {'toronto' : 105,'rochester' : 64,'cleveland' : 191},
+        'calgary' : {'vancouver': 605,'winnipeg' : 829},
+        'charlotte' : {'greensboro' : 91, 'augusta' : 161},
+        'chattanooga' : {'nashville' : 129, 'atlanta' : 117},
+        'chicago' : {'milwaukee': 90,'midland': 279},
+        'cincinnati' : {'indianapolis' : 110,'dayton' : 56},
+        'cleveland' : {'pittsburgh' : 157,'columbus' : 142, 'buffalo': 191},
+        'coloradoSprings' : {'denver' : 792,'santaFe' : 83},
+        'columbus' : {'dayton' : 72, 'cleveland' : 142},
+        'dallas' : {'denver' : 792,'mexia' : 83},
+        'dayton' : {'cincinnati': 56, 'columbus': 72},
+        'daytonaBeach' : {'jacksonville' : 92,'orlando' : 54},
+        'denver' : {'wichita' : 523,'grandJunction' : 246, 'coloradoSprings' : 792, 'dallas': 792},
+        'desMoines' : {'omaha' : 135,'minneapolis' : 246},
+        'elPaso' : {'sanAntonio' : 580,'tucson' : 320, 'albuquerque' : 267},
+        'eugene' : {'salem' : 63,'medford' : 165},
+        'europe' : {'philadelphia': 3939},
+        'ftWorth' : {'oklahomaCity' : 209},
+        'fresno' : {'modesto' : 109, 'bakersfield' : 107},
+        'grandJunction' : {'provo' : 220, 'denver': 246},
+        'greenBay' : {'minneapolis' : 304, 'milwaukee' : 117},
+        'greensboro' : {'raleigh' : 74, 'charlotte': 91},
+        'houston' : {'mexia' : 165, 'austin' : 186, 'beaumont': 69},
+        'indianapolis' : {'stLouis' : 246, 'cincinnati':110},
+        'jacksonville' : {'savannah' : 140, 'lakeCity' : 113, 'daytonaBeach': 92},
+        'japan' : {'pointReyes' : 5131,'sanLuisObispo' : 5451},
+        'kansasCity' : {'tulsa' : 249,'stLouis' : 256,'wichita' : 190},
+        'keyWest' : {'tampa' : 446},
+        'lafayette' :{'batonRouge' : 50, 'beaumont': 122},
+        'lakeCity' : {'tampa' : 169,'tallahassee' : 104, 'jacksonville': 113},
+        'laredo' : {'sanAntonio' : 154,'mexico' : 741},
+        'lasVegas' : {'losAngeles' : 275,'saltLakeCity' : 486},
+        'lincoln' : {'wichita' : 277,'omaha' : 58},
+        'littleRock' : {'memphis' : 137,'tulsa' : 276},
+        'losAngeles' : {'sanDiego' : 124,'sanLuisObispo' : 182, 'bakersfield': 112, 'lasVegas': 275},
+        'macon' : {'albanyGA' : 106, 'atlanta' : 82},
+        'medford' : {'redding' : 150 , 'eugene': 165},
+        'memphis' : {'nashville' : 210, 'littleRock': 137},
+        'mexia' : {'dallas': 83, 'houston': 165},
+        'mexico' : {'laredo': 741},
+        'miami' : {'westPalmBeach' : 67},
+        'midland' : {'toledo' : 82, 'chicago': 279},
+        'milwaukee' : {'chicago' :90, 'greenBay': 117},
+        'minneapolis' : {'winnipeg' : 463, 'desMoines': 246, 'greenBay': 304},
+        'modesto' : {'stockton' : 29, 'fresno': 109},
+        'montreal' : {'ottawa' : 132, 'albanyNY' : 226},
+        'nashville' : {'chattanooga' :129, 'memphis' : 210},
+        'newHaven' : {'providence' : 110,'stamford' : 92},
+        'newOrleans' : {'pensacola' : 268, 'batonRouge' : 80},
+        'newYork' : {'philadelphia' : 101},
+        'norfolk' : {'richmond' : 92,'raleigh' : 174},
+        'oakland' : {'sanFrancisco' : 8,'sanJose' : 42},
+        'oklahomaCity' : {'tulsa' : 105, 'ftWorth': 209},
+        'omaha' : {'desMoines' : 135, 'lincoln': 58},
+        'orlando' :{'westPalmBeach' : 168,'tampa' : 84, 'daytonaBeach': 54},
+        'ottawa' : {'toronto' : 269, 'montreal': 132},
+        'pensacola' : {'tallahassee' : 120, 'newOrleans': 268},
+        'philadelphia' : {'pittsburgh' : 319,'newYork' : 101,'uk1' : 3548,'uk2' : 3548, 'baltimore': 102, 'europe': 3939},
+        'phoenix' : {'tucson' : 117,'yuma' : 178},
+        'pittsburgh' :{'cleveland' :157, 'philadelphia': 319},
+        'pointReyes' : {'redding' : 215,'sacramento' : 115, 'japan' : 5131},
+        'portland' : {'seattle' : 174,'salem' : 47, 'boise' : 428},
+        'providence' : {'boston': 51, 'newHaven' : 110},
+        'provo' : {'grandJunction': 220},
+        'raleigh' : {'greensboro': 74, 'norfolk': 174},
+        'redding' : {'medford': 150, 'pointReyes' : 215},
+        'reno' : {'saltLakeCity' : 520,'sacramento' : 133},
+        'richmond' : {'washington' : 105, 'norfolk': 92},
+        'rochester' : {'albanyNY' :148, 'buffalo': 64},
+        'sacramento' : {'sanFrancisco' : 95,'stockton' : 51, 'pointReyes': 115, 'reno' : 133},
+        'salem' : {'eugene' : 63, 'portland': 47},
+        'salinas' : {'sanJose' : 31,'sanLuisObispo' : 137},
+        'saltLakeCity' : {'boise': 349, 'lasVegas' : 486, 'reno': 520},
+        'sanAntonio' : {'austin' : 79, 'elPaso': 580, 'laredo' : 154},
+        'sanDiego' : {'yuma' : 172, 'losAngeles':124},
+        'sanFrancisco' : {'oakland' : 8, 'sacramento' : 95},
+        'sanJose' : {'salinas': 31},
+        'santaFe' : {'albuquerque' : 61, 'coloradoSprings': 83},
+        'saultSteMarie' : {'thunderBay' : 442,'toronto' : 436},
+        'sanLuisObispo' : {'japan' : 5451, 'losAngeles': 182, 'salinas':137},
+        'savannah' :{'augusta' : 131, 'jacksonville' : 140},
+        'seattle' : {'vancouver' : 115, 'portland' : 174},
+        'stamford' : {'newHaven' : 92},
+        'stLouis' : {'indianapolis' : 246, 'kansasCity' : 256},
+        'stockton' : {'modesto' : 29, 'sacramento' : 51},
+        'tallahassee' : {'albanyGA' : 120, 'lakeCity' : 104, 'pensacola': 120},
+        'tampa' : {'keyWest' : 446, 'lakeCity' : 169, 'orlando': 84},
+        'thunderBay' : {'winnipeg' : 440, 'saultSteMarie': 442},
+        'toledo' : {'midland': 82},
+        'toronto' : {'buffalo' : 105, 'ottawa': 269, 'saultSteMarie': 436},
+        'tucson' : {'elPaso': 320, 'phoenix': 117},
+        'tulsa' : {'kansasCity' : 249, 'littleRock': 276, 'oklahomaCity': 105},
+        'uk1' : {'philadelphia': 3548},
+        'uk2' : {'philadelphia' : 3548},
+        'vancouver' :{'calgary' :605, 'seattle' : 115},
+        'washington' : {'baltimore' : 45, 'richmond': 105},
+        'westPalmBeach' : {'miami' : 67, 'orlando': 168},
+        'wichita' : {'denver': 523, 'kansasCity': 190, 'lincoln': 277},
+        'winnipeg' : {'calgary':829, 'minneapolis': 463,'thunderBay': 440},
+        'yuma' : {'phoenix': 178, 'sanDiego': 172}}
+
+coordinate = {
+'albanyGA' :(31.58,84.17),
+'albanyNY': (42.66,73.78),
+'albuquerque' :(35.11,106.61),
+'atlanta' :(33.76,84.4),
+'augusta' :(33.43,82.02),
+'austin' :(30.3,97.75),
+'bakersfield' :(35.36,119.03),
+'baltimore' :(39.31,76.62),
+'batonRouge' :(30.46,91.14),
+'beaumont' :(30.08,94.13),
+'boise' :(43.61,116.24),
+'boston' :(42.32,71.09),
+'buffalo' :(42.9,78.85),
+'calgary' :(51,114),
+'charlotte' :(35.21,80.83),
+'chattanooga' :(35.05,85.27),
+'chicago' :(41.84,87.68),
+'cincinnati' :(39.14,84.5),
+'cleveland' :(41.48,81.67),
+'coloradoSprings' :(38.86,104.79),
+'columbus' :(39.99,82.99),
+'dallas' :(32.8,96.79),
+'dayton' :(39.76,84.2),
+'daytonaBeach' :(29.21,81.04),
+'denver' :(39.73,104.97),
+'desMoines' :(41.59,93.62),
+'elPaso' :(31.79,106.42),
+'eugene' :(44.06,123.11),
+'europe' :(48.87,-2.33),
+'ftWorth' :(32.74,97.33),
+'fresno' :(36.78,119.79),
+'grandJunction' :(39.08,108.56),
+'greenBay' :(44.51,88.02),
+'greensboro' :(36.08,79.82),
+'houston' :(29.76,95.38),
+'indianapolis' :(39.79,86.15),
+'jacksonville' :(30.32,81.66),
+'japan' :(35.68,220.23),
+'kansasCity' :(39.08,94.56),
+'keyWest' :(24.56,81.78),
+'lafayette' :(30.21,92.03),
+'lakeCity' :(30.19,82.64),
+'laredo' :(27.52,99.49),
+'lasVegas' :(36.19,115.22),
+'lincoln' :(40.81,96.68),
+'littleRock' :(34.74,92.33),
+'losAngeles' :(34.03,118.17),
+'macon' :(32.83,83.65),
+'medford' :(42.33,122.86),
+'memphis' :(35.12,89.97),
+'mexia' :(31.68,96.48),
+'mexico' :(19.4,99.12),
+'miami' :(25.79,80.22),
+'midland' :(43.62,84.23),
+'milwaukee' :(43.05,87.96),
+'minneapolis' :(44.96,93.27),
+'modesto' :(37.66,120.99),
+'montreal' :(45.5,73.67),
+'nashville' :(36.15,86.76),
+'newHaven' :(41.31,72.92),
+'newOrleans' :(29.97,90.06),
+'newYork' :(40.7,73.92),
+'norfolk' :(36.89,76.26),
+'oakland' :(37.8,122.23),
+'oklahomaCity' :(35.48,97.53),
+'omaha' :(41.26,96.01),
+'orlando' :(28.53,81.38),
+'ottawa' :(45.42,75.69),
+'pensacola' :(30.44,87.21),
+'philadelphia' :(40.72,76.12),
+'phoenix' :(33.53,112.08),
+'pittsburgh' :(40.4,79.84),
+'pointReyes' :(38.07,122.81),
+'portland' :(45.52,122.64),
+'providence' :(41.8,71.36),
+'provo' :(40.24,111.66),
+'raleigh' :(35.82,78.64),
+'redding' :(40.58,122.37),
+'reno' :(39.53,119.82),
+'richmond' :(37.54,77.46),
+'rochester' :(43.17,77.61),
+'sacramento' :(38.56,121.47),
+'salem' :(44.93,123.03),
+'salinas' :(36.68,121.64),
+'saltLakeCity' :(40.75,111.89),
+'sanAntonio' :(29.45,98.51),
+'sanDiego' :(32.78,117.15),
+'sanFrancisco' :(37.76,122.44),
+'sanJose' :(37.3,121.87),
+'sanLuisObispo' :(35.27,120.66),
+'santaFe' :(35.67,105.96),
+'saultSteMarie' :(46.49,84.35),
+'savannah' :(32.05,81.1),
+'seattle' :(47.63,122.33),
+'stLouis' :(38.63,90.24),
+'stamford' :(41.07,73.54),
+'stockton' :(37.98,121.3),
+'tallahassee' :(30.45,84.27),
+'tampa' :(27.97,82.46),
+'thunderBay' :(48.38,89.25),
+'toledo' :(41.67,83.58),
+'toronto' :(43.65,79.38),
+'tucson' :(32.21,110.92),
+'tulsa' :(36.13,95.94),
+'uk1' :(51.3,0),
+'uk2' :(51.3,0),
+'vancouver'	:(49.25,123.1),
+'washington' :(38.91,77.01),
+'westPalmBeach' :(26.71,80.05),
+'wichita': (37.69,97.34),
+'winnipeg' : (49.9,97.13),
+'yuma': (32.69,114.62)}
+
+import math
+from collections import OrderedDict
+
+#heuristic function
+
+def h(child,goal):
+    x2, y2 = coordinate[goal]
+    x1, y1 = coordinate[child]
+    return math.sqrt((69.5 * (x1 - x2)) ** 2 + (69.5 * math.acos((x1 + x2) / 360 * 3.14) * (y1 - y2)) ** 2)
+
+#initialization
+
+def astar(start, goal):
+    path_cost = {}
+    parent = {}
+    frontier = graph
+    infinity = 999999
+    path = []
+    heuristic = {}
+    visited = []
+    expanded = []
+
+
+    for node in frontier:
+        path_cost[node] = infinity           #initializing path cost of all nodes to a large value
+        heuristic[node] = h(node, goal)      #initializing heuristic distances of nodes
+        path_cost[start] = 0                 #initializing path cost of start to 0
+
+
+    while frontier:
+        minNode = None                      #initalize minNode to null
+        if start in frontier:               #to find minNode
+            minNode = start
+        else:
+            x = {}
+            for node in frontier:
+                if node in visited:
+                    x[node] = heuristic[node] + path_cost[node]     #if node is not start, then a minNode is the minimum of : all the nodes in visited + the heuristic distance of the node from the goal
+            minNode = min(x, key=x.get)
+        for child, weight in graph[minNode].items():
+            if weight + path_cost[minNode] < path_cost[child]:
+                path_cost[child] = weight + path_cost[minNode]      #update path cost of the children of minNode
+                parent[child] = minNode                             #assign the minNode to a parent dictionary of value=child and key as minNode
+            visited.append(child)
+        frontier.pop(minNode)                  #remove the minNode from the dictionary of nodes
+
+    current = goal
+    expanded.append(start)
+    expanded.append(goal)
+
+    while current != start:                     #backtrack the path from goal
+        try:
+            path.append(current)
+            for child,dad in parent.items():   #in order to find the nodes expanded
+                if current == dad:
+                    expanded.append(dad)    #append the children and their parents into the expanded list if they lie in the path
+                    expanded.append(child)
+            current = parent[current]           #the parent dictionary contains the parent node of the nodes
+
+        except KeyError:
+            print("Path unreachable")
+            break
+    path.append(start)
+
+    if path_cost[goal] != infinity:              #this would be possible if the goal wasn't reached at all, so it remains infinty as assigned earlier
+        print("shortest distance is " + str(path_cost[goal]))
+        print("path is " + str(path[::-1]))
+        print("number of nodes in the path " + str(len(path[::-1])))
+        print("visited list " + str(expanded))
+        print("number of visited nodes " + str(len(expanded)))
+
